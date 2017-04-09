872b651b1e45f939773ad0f99c195b7a1bba96cd
Samuel <Samuel@LAPTOP-FQSMTE4G.lan>
2017-04-09 18:52:34 -0500

    Añadi el formulario login, unicamente con los text y un boton

--- src/com/lords/views/Login.java
+++ src/com/lords/views/Login.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField jtfUsuario;
+	private JPasswordField jpfPassword;
 
 	/**
 	 * Launch the application.
@@ -32,11 +37,27 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 406);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		jtfUsuario = new JTextField();
+		jtfUsuario.setBounds(144, 150, 117, 20);
+		panel.add(jtfUsuario);
+		jtfUsuario.setColumns(10);
+		
+		JButton btnAcceder = new JButton("ACCEDER");
+		btnAcceder.setBounds(159, 277, 89, 23);
+		panel.add(btnAcceder);
+		
+		jpfPassword = new JPasswordField();
+		jpfPassword.setBounds(144, 205, 117, 20);
+		panel.add(jpfPassword);
 	}
-
 }
