bd02182450c5f0bad30be4646305614de58f8d56
Samuel <Samuel@LAPTOP-FQSMTE4G.lan>
2017-04-09 17:17:43 -0500

    Agregue una linea mas de mensaje

--- src/com/lords/verifact/main.java
+++ src/com/lords/verifact/main.java
@@ -6,6 +6,8 @@ public class main {
 	public static void main(String[] args) {
 System.out.println("Hola developers");
 System.out.println("Hola desde Zero");
+System.out.println("PUEDES VER ESTO VIC?");
 	}
 
 }
+
