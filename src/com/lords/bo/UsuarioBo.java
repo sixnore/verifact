0a8f28e1f6ed82e53439569250e1ab6bdd4aef16
LUISA <LUISA@192.168.80.1>
2017-04-09 20:18:41 -0500

    Dao validacion longitud

--- src/com/lords/bo/UsuarioBo.java
+++ src/com/lords/bo/UsuarioBo.java
@@ -1,5 +1,7 @@
 package com.lords.bo;
 
+import javax.swing.JOptionPane;
+
 import com.lords.model.UsuarioModel;
 
 public class UsuarioBo {
@@ -7,6 +9,16 @@ public class UsuarioBo {
 	public String acceder(UsuarioModel usuarioModelo) {
 		String mensaje="";
 		mensaje=usuarioModelo.getUsername()+" "+usuarioModelo.getPassword();
+		
+		String username = usuarioModelo.getUsername();
+		String password = usuarioModelo.getPassword();
+		
+		
+		if(!(username.length()>6&&username.length()<20||password.length()>8&&password.length()<20)){
+			JOptionPane.showMessageDialog(null, "No cumple con los requisitos");
+			
+		}
+		
 		return mensaje;
 	}
 
