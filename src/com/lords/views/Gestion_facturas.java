200ebb097d383c1697f4f3d7671f389d6137c850
Samuel <Samuel@LAPTOP-FQSMTE4G>
2017-04-10 12:18:28 -0500

    Jcalendar en la paleta de swing

--- src/com/lords/views/Gestion_facturas.java
+++ src/com/lords/views/Gestion_facturas.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Gestion_facturas extends JFrame {
 
@@ -32,11 +35,24 @@ public class Gestion_facturas extends JFrame {
 	 */
 	public Gestion_facturas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 414);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblEstado = new JLabel("Estado:");
+		lblEstado.setBounds(46, 48, 46, 14);
+		panel.add(lblEstado);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Estado", "En espera", "Entregada", "Fallo"}));
+		comboBox.setToolTipText("");
+		comboBox.setBounds(224, 77, 109, 20);
+		panel.add(comboBox);
 	}
-
 }
